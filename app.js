8695de3eb6d7f76b0b301cf31039c44d75d9f75f
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2024-01-03 23:24:21 +0500

    js-simple-website

---
+++ app.js
@@ -0,0 +1,6 @@
+let a = document.getElementById("btn");
+let b = document.getElementById("light");
+a.addEventListener('click',function(){
+  btn.classList.toggle('active');
+  light.classList.toggle("on");
+})
